71e0a7603708bd6f5528c43647aa21f67d7d5d29
Jamiu Mojeed <mojjam07@gmail.com>
2024-09-22 13:12:06 +0100

    after reading about static files

--- backview/project/settings.py
+++ backview/project/settings.py
@@ -225,9 +225,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
