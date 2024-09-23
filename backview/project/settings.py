241a49b12bb3b1677246b93434cf2ecba4573e2b
Jamiu Mojeed <mojjam07@gmail.com>
2024-09-23 02:18:30 +0100

    static file issue check

--- backview/project/settings.py
+++ backview/project/settings.py
@@ -303,8 +303,8 @@ USE_TZ = True
 # Static and media files settings
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'backview/static')]
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, './backview/static')]
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Enable WhiteNoise to compress and cache static files
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
