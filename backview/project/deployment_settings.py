5baca72baaad95e89af20acb93cb14da139c7a6c
Jamiu Mojeed <mojjam07@gmail.com>
2024-11-20 13:04:35 +0100

    deployment settingss

--- backview/project/deployment_settings.py
+++ backview/project/deployment_settings.py
@@ -4,7 +4,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
 
 DEBUG = False
 SECRET_KEY = os.environ.get['SECRET_KEY']
