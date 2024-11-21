2da8effb5de2d660335aa06951863f5497876aff
Jamiu Mojeed <mojjam07@gmail.com>
2024-11-21 16:20:13 +0100

    APIView

--- backview/api/urls.py
+++ backview/api/urls.py
@@ -5,8 +5,7 @@ from .views import (
     ContactViewSet,
     ConsultViewSet,
     QuestionViewSet,
-    ApprovedTestimonialListView,
-    SubmitTestimonialView,
+    TestimonialListView
 )
 
 router = DefaultRouter()
@@ -14,10 +13,8 @@ router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'contacts', ContactViewSet, basename='contact')
 router.register(r'consults', ConsultViewSet, basename='consult')
 router.register(r'questions', QuestionViewSet, basename='question')
-router.register(r'testimonials', QuestionViewSet, basename='testimonials')
+router.register(r'testimonials', TestimonialListView.as_view(), name='testimonials')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/testimonials/approved/', ApprovedTestimonialListView.as_view(), name='approved_testimonials'),
-    path('api/testimonials/submit/', SubmitTestimonialView.as_view(), name='submit_testimonial'),
 ]
