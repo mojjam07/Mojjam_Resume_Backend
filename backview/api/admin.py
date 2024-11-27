c5683e72c89d581d78f2c1f7ff6fb45f1ddaf712
Jamiu Mojeed <mojjam07@gmail.com>
2024-11-27 13:43:10 +0100

    followed a guide

--- backview/api/admin.py
+++ backview/api/admin.py
@@ -3,7 +3,6 @@ from .models import Project, Contact, Testimonial, Consult, Question
 
 # Register your models here.
 admin.site.register(Project)
-
 admin.site.register(Contact)
 admin.site.register(Testimonial)
 admin.site.register(Consult)
